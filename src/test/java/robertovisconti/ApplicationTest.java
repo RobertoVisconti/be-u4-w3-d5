aa505d5065c8ab198d35aff6e15a2c3cc7a7b1a9
Roberto Visconti <robertovisconti93@gmail.com>
2026-06-21 11:27:10 +0200

    Initial commit

---
+++ src/test/java/robertovisconti/ApplicationTest.java
@@ -0,0 +1,13 @@
+package robertovisconti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
